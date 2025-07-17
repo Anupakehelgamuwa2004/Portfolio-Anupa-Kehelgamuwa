edd5c83e2a68c35a298448bd0a767ea010dcb3b4
Anupakehelgamuwa2004 <anupakehel8403@gmail.com>
2025-07-17 12:49:53 +0530

    toest message

--- PortfolioWebsite/protect.js
+++ PortfolioWebsite/protect.js
@@ -1,7 +1,7 @@
 // Disable right-click context menu
 window.addEventListener('contextmenu', function(e) {
     e.preventDefault();
-    alert('Inspecting or copying code is disabled on this site.');
+    alert('Sorry, not available! 😅');
 });
 
 // Disable common inspect shortcuts
@@ -9,12 +9,12 @@ window.addEventListener('keydown', function(e) {
     // F12
     if (e.key === 'F12') {
         e.preventDefault();
-        alert('Inspecting or copying code is disabled on this site.');
+        alert('Sorry, not available! 😅');
     }
     // Ctrl+Shift+I, Ctrl+Shift+C, Ctrl+U
     if ((e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'C')) ||
         (e.ctrlKey && e.key === 'u')) {
         e.preventDefault();
-        alert('Inspecting or copying code is disabled on this site.');
+        alert('Sorry, not available! 😅');
     }
 }); 
